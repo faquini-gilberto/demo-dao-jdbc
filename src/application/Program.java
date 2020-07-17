369c40ea3de3fff74eb399978a1d961b0f0e6cdf
faquini-gilberto <faquini.gilberto@gmail.com>
2020-07-17 15:17:33 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Teste");
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = new Seller(23, "Gilberto", "giba@gmail", new Date(), 3000.0, obj);
 		System.out.println(seller);
