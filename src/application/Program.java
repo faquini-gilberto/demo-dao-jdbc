92b74d46b46c9d1fc3722abdfad91c26e77327a5
faquini-gilberto <faquini.gilberto@gmail.com>
2020-07-20 11:40:39 -0300

    Insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -13,19 +14,27 @@ public class Program {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
+		System.out.println("-- Find ById      --");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		System.out.println("--------------------");
+		System.out.println("--FindByDepartment--");
 
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
 
-		System.out.println("--------------------");
+		System.out.println("--    FindAll    --");
 
 		List<Seller> list1 = sellerDao.findAll();
 		list1.forEach(System.out::println);
+		
 
+		System.out.println("--    Insert    --");
+		
+		seller = new Seller(null, "Shark", "shark@gmail.com", new Date(), 2500.90, department);
+		sellerDao.insert(seller);
+		System.out.println("Registro adicionado. " + seller.getId());;
+		
 	}
 
 }
